cae810e5684a6284dde6b73d8481374a6f93da6e
RahelJessy111 <94190233+RahelJessy111@users.noreply.github.com>
2021-11-15 17:21:20 +0700

    Add files via upload

---
+++ RahelJessy.py
@@ -0,0 +1,9 @@
+perkenalan = [
+     "Nama      : Rahel Jessy Yunita",
+     "NIM       : 212410102083 ",
+     "Kelompok  : 28 ",
+     "Prodi     : Teknologi Informasi",
+     "Fakultas  : Ilmu Komputer"]
+
+for i in perkenalan :
+    print (i)
